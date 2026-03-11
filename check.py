a54d17fa6b4914a96f9725d84e5552d4f6cdaef5
emiinoue <aya_ino07@hotmail.com>
2026-03-11 20:31:45 +0900

    Update check.py

--- check.py
+++ check.py
@@ -1,1 +1,38 @@
+import requests
 
+URL = "https://www.apple.com/jp/shop/refurbished/ipad"
+
+LINE_TOKEN = "ここにLINEトークン"
+USER_ID = "ここにUserID"
+
+def send_line(msg):
+
+    url = "https://api.line.me/v2/bot/message/push"
+
+    headers = {
+        "Authorization": f"Bearer {LINE_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "to": USER_ID,
+        "messages":[
+            {
+                "type":"text",
+                "text": msg
+            }
+        ]
+    }
+
+    requests.post(url, headers=headers, json=data)
+
+
+def check():
+
+    r = requests.get(URL)
+
+    if "iPad mini" in r.text:
+        send_line("iPad mini 整備品が出ました！")
+
+
+check()
